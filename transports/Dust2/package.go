7af8c3f18dbf60b088826c4fe42bb47fed941387
Drake Wilson <drake@dasyatidae.com>
2015-04-20 07:42:10 -0500

    Dust2: Translate exposed obfs4proxy log level to go-logging log level
    Approximately, at least.  obfs4proxy seems to consider NOTICE to be
    higher than ERROR, but the important thresholds are around the INFO
    and DEBUG range anyway, so that's probably fine.

--- transports/Dust2/package.go
+++ transports/Dust2/package.go
@@ -1,20 +1,14 @@
 package Dust2
 
 import (
-	"os"
-
 	"github.com/op/go-logging"
 
 	obfs4log "git.torproject.org/pluggable-transports/obfs4.git/common/log"
 	"git.torproject.org/pluggable-transports/obfs4.git/transports/base"
-
-	"github.com/blanu/Dust/go/v2/interface"
 )
 
 const (
 	transportName = "Dust2"
-
-	envvarDebug = "OBFS4PROXY_DUST_DEBUG"
 )
 
 var log = logging.MustGetLogger("transport/Dust2")
@@ -47,21 +41,28 @@ func (_ *obfs4logBackend) Log(level logging.Level, calldepth int, rec *logging.R
 	return nil
 }
 
+func translateLevel(level int) logging.Level {
+	switch level {
+	case obfs4log.LevelError, obfs4log.LevelWarn:
+		// obfs4proxy considers NOTICE to be above ERROR/WARNING for some reason, so
+		// we have to let it do the filtering here.  Hrgh.
+		return logging.NOTICE
+	case obfs4log.LevelInfo:
+		return logging.INFO
+	case obfs4log.LevelDebug:
+		return logging.DEBUG
+	default:
+		// Hmm.
+		return logging.NOTICE
+	}
+}
+
 func init() {
 	backend := &obfs4logBackend{}
 	formatSpec := "%{module:s}: %{message}"
 	formatter := logging.MustStringFormatter(formatSpec)
 	formatted := logging.NewBackendFormatter(backend, formatter)
 	leveled := logging.AddModuleLevel(formatted)
-
-	// TODO: actually pick up log level... ?!
-	leveled.SetLevel(logging.NOTICE, "")
-
-	if os.Getenv(envvarDebug) == "1" {
-		for _, module := range Dust.LogModules {
-			leveled.SetLevel(logging.DEBUG, module)
-		}
-	}
-
+	leveled.SetLevel(translateLevel(obfs4log.Level()), "")
 	logging.SetBackend(leveled)
 }
