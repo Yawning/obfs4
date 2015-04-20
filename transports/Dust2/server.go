e71725438ce20bc100781dc03eab080801ae5454
Drake Wilson <drake@dasyatidae.com>
2015-04-20 07:33:33 -0500

    Dust2: Log bridge line when generating new identity
    (obfs4 writes it to a file, though.  Hmm.)

--- transports/Dust2/server.go
+++ transports/Dust2/server.go
@@ -1,9 +1,12 @@
 package Dust2
 
 import (
+	"fmt"
 	"net"
 	"os"
 	"path/filepath"
+	"sort"
+	"strings"
 
 	"git.torproject.org/pluggable-transports/goptlib.git"
 	"git.torproject.org/pluggable-transports/obfs4.git/transports/base"
@@ -18,6 +21,48 @@ type serverFactory struct {
 	transport *Transport
 	stateDir  string
 	private   *Dust.ServerPrivate
+}
+
+// lazyBridgeLine lazily wraps a public server identity for logging display as a bridge line.
+type lazyBridgeLine struct {
+	public *Dust.ServerPublic
+}
+
+func (lazy lazyBridgeLine) String() string {
+	unparsed := lazy.public.Unparse()
+
+	// TODO: lock down spec so that values can't contain horizontal whitespace.  (They
+	// don't currently, but it should be documented.)
+	parts := make([]string, 0, len(unparsed))
+	for key, val := range unparsed {
+		parts = append(parts, key+"="+val)
+	}
+	sort.Strings(parts)
+
+	return fmt.Sprintf("Bridge %s ADDRESS %s", transportName, strings.Join(parts, " "))
+}
+
+func writeNewIdentity(unparsed map[string]string, idPath string) (private *Dust.ServerPrivate, err error) {
+	ep, err := Dust.ParseEndpointParams(unparsed)
+	if err != nil {
+		log.Error("parsing endpoint parameters: %s", err)
+		return
+	}
+
+	private, err = Dust.NewServerPrivate(ep)
+	if err != nil {
+		log.Error("generating new identity: %s", err)
+		return
+	}
+
+	err = private.SavePrivateFile(idPath)
+	if err != nil {
+		log.Error("saving new identity to %s: %s", idPath, err)
+		return
+	}
+
+	log.Notice("New bridge identity!  %s", &lazyBridgeLine{private.Public()})
+	return
 }
 
 func (t *Transport) ServerFactory(stateDir string, args *pt.Args) (base.ServerFactory, error) {
@@ -44,21 +89,9 @@ func (t *Transport) ServerFactory(stateDir string, args *pt.Args) (base.ServerFa
 
 	case statErr != nil && os.IsNotExist(statErr):
 		// ID file doesn't exist.  Try to write a new one.
-		ep, err := Dust.ParseEndpointParams(unparsed)
+		private, err = writeNewIdentity(unparsed, idPath)
 		if err != nil {
-			log.Error("parsing endpoint parameters: %s", err)
-			return nil, err
-		}
-
-		private, err = Dust.NewServerPrivate(ep)
-		if err != nil {
-			log.Error("generating new identity: %s", err)
-			return nil, err
-		}
-
-		err = private.SavePrivateFile(idPath)
-		if err != nil {
-			log.Error("saving new identity to %s: %s", idPath, err)
+			// We already logged the error in writeNewIdentity.
 			return nil, err
 		}
 		
