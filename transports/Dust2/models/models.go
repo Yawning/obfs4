9a743bd0489798f5c5f15d3863be7e4d204b8ff5
Brandon Wiley <brandon@blanu.net>
2015-05-25 13:17:11 -0500

    Clamped Poisson paramters

--- transports/Dust2/models/models.go
+++ transports/Dust2/models/models.go
@@ -1,9 +1,11 @@
 package models
 
 import (
-	_ "git.torproject.org/pluggable-transports/obfs4.git/transports/Dust2/models/HTTPS"
-
 	_ "git.torproject.org/pluggable-transports/obfs4.git/transports/Dust2/models/HTTP"
 
+	_ "git.torproject.org/pluggable-transports/obfs4.git/transports/Dust2/models/HTTPS"
+
 	_ "git.torproject.org/pluggable-transports/obfs4.git/transports/Dust2/models/RTSP"
+
+	_ "git.torproject.org/pluggable-transports/obfs4.git/transports/Dust2/models/Tor"
 )
