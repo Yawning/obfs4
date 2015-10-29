611205be681322883a4d73dd00fcb13c4352fe53
Yawning Angel <yawning@torproject.org>
2015-10-29 17:29:21 +0000

    Add the "meek_lite" transport, which does what one would expect.
    This is a meek client only implementation, with the following
    differences with dcf's `meek-client`:
    
     - It is named `meek_lite` to differentiate it from the real thing.
     - It does not support using an external helper to normalize TLS
       signatures, so adversaries can look for someone using the Go
       TLS library to do HTTP.
     - It does the right thing with TOR_PT_PROXY, even when a helper is
       not present.
    
    Most of the credit goes to dcf, who's code I librerally cribbed and
    stole.  It is intended primarily as a "better than nothina" option
    for enviornments that do not or can not presently use an external
    Firefox helper.

--- transports/transports.go
+++ transports/transports.go
@@ -34,6 +34,7 @@ import (
 	"sync"
 
 	"git.torproject.org/pluggable-transports/obfs4.git/transports/base"
+	"git.torproject.org/pluggable-transports/obfs4.git/transports/meeklite"
 	"git.torproject.org/pluggable-transports/obfs4.git/transports/obfs2"
 	"git.torproject.org/pluggable-transports/obfs4.git/transports/obfs3"
 	"git.torproject.org/pluggable-transports/obfs4.git/transports/obfs4"
@@ -83,6 +84,7 @@ func Get(name string) base.Transport {
 
 // Init initializes all of the integrated transports.
 func Init() error {
+	Register(new(meeklite.Transport))
 	Register(new(obfs2.Transport))
 	Register(new(obfs3.Transport))
 	Register(new(obfs4.Transport))
